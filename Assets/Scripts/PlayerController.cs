4651ddc4308dc3b4af7236f6135f34dd5705d742
Matt Talubas <dmtmanga@gmail.com>
2018-09-01 00:52:45 -0400

    Initial Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    #region Fields
+
+    // Public
+    public Laser _laser;
+
+    // Private
+    private int _numOfEngineers;
+
+    #endregion // Fields
+
+
+    #region Methods
+
+    // Use this for initialization
+    void Start () {
+        _numOfEngineers = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    #endregion // Methods
+
+
+
+
+
+}
---
+++ Assets/Scripts/SpawnController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour {
+
+    public SpawnPoint[] _spawnPoint = new SpawnPoint[6];
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
